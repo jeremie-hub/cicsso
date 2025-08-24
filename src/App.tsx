fa9894eccd2e51c78c8f04d32bc04c335b28657e
JYRYMY ROBS <jeremieregencia@gmail.com>
2025-08-24 22:03:02 +0800

    Fix router basename and vite config for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/cicsso">
           <Layout>
             <Routes>
               <Route path="/" element={<Home />} />
